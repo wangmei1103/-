69ffc3cee788c23a889094a1a9196fc37c5cf5e4
wangmei <2910602199@qq.com>
2020-12-10 11:14:15 +0800

    ds

--- DS/数据结构/20_12_07/20_12_07/list.c
+++ DS/数据结构/20_12_07/20_12_07/list.c
@@ -1,6 +1,7 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<assert.h>
 //#include"list.h"
 
 #define SLTDateType int
@@ -32,27 +33,124 @@ void SListPrint(SListNode* plist)
 // 单链表尾插
 void SListPushBack(SListNode** pplist, SLTDateType x)
 {
-	//assert(*pplist != NULL);
+	SListNode* newNode=BySListNode(x);
+	if (*pplist==NULL)
+		*pplist=newNode;
+	else{
+		SListNode* tail=*pplist;
+		while(tail->next!=NULL)
+		{
+			tail=tail->next;
+		}
+		tail->next=newNode;
+	}
 }
 
-
 // 单链表的头插
-void SListPushFront(SListNode** pplist, SLTDateType x);
+//void SListPushFront(SListNode** pplist, SLTDateType x)
+//{
+//	assert(pplist!=NULL);
+//	SListNode* newNode=BySListNode(x);
+//	if(*pplist==NULL)
+//		*pplist=newNode;
+//	else{
+//		newNode->next=*pplist;
+//		*pplist=newNode;
+//	}
+//}
+
 // 单链表的尾删
-void SListPopBack(SListNode** pplist);
+void SListPopBack(SListNode** pplist)
+{
+	SListNode* prev=NULL;
+	SListNode* tail=*pplist;
+	if(tail==NULL||tail->next==NULL){
+		free(tail);
+		*pplist=NULL;
+	}
+	else{
+		while(tail->next)
+		{
+			prev=tail;
+			tail=tail->next;
+		}
+		free(tail);
+		tail=NULL;
+		prev->next=NULL;
+	}
+}
+
 // 单链表头删
-void SListPopFront(SListNode** pplist);
+void SListPopFront(SListNode** pplist)
+{
+	SListNode* newNode=*pplist;
+	if(newNode==NULL)
+		return;
+	else if(newNode->next==NULL)
+	{
+		free(newNode);
+		*pplist=NULL;
+	}
+	else{
+		SListNode* next=newNode->next;
+		free(newNode);
+		*pplist=next;
+	}
+	/*assert(*pplist!=NULL);
+	SListNode* newNode=*pplist;
+	if(newNode!=NULL)
+	{
+		*pplist=newNode->next;
+		free(newNode);
+	}*/
+}
 // 单链表查找
-SListNode* SListFind(SListNode* plist, SLTDateType x);
+SListNode* SListFind(SListNode* plist, SLTDateType x)
+{
+	SListNode* newNode=plist;
+	while(newNode!=NULL && newNode->data!=x)
+		newNode=newNode->next;
+	return newNode;
+}
+
 // 单链表在pos位置之后插入x
-// 分析思考为什么不在pos位置之前插入？
-void SListInsertAfter(SListNode* pos, SLTDateType x);
+void SListInsertAfter(SListNode* pos, SLTDateType x)
+{
+	assert(pos!=NULL);
+	SListNode* next = pos->next;
+	SListNode* newNode=BySListNode(x);
+	pos->next = newNode;
+	newNode->next = next;
+}
+
 // 单链表删除pos位置之后的值
 // 分析思考为什么不删除pos位置？
-void SListEraseAfter(SListNode* pos);
+void SListEraseAfter(SListNode* pos)
+{
+	assert(pos!=NULL);
+	SListNode* next = pos->next;
+	if (next != NULL)
+	{
+		SListNode* nextnext = next->next;
+		free(next);
+		pos->next = nextnext;
+	}
+}
 // 单链表的销毁
-void SListDestory(SListNode* plist);
- 
+void SListDestory(SListNode* plist)
+{
+	assert(plist!=NULL);
+	SListNode* newNode= plist;
+	SListNode* p=newNode->next;
+	newNode->next=NULL;
+	while(p!=NULL)
+	{
+		newNode=p;
+		p=p->next;
+		newNode->next=plist;
+		plist=newNode;
+	}
+}
 
 int main()
 {
