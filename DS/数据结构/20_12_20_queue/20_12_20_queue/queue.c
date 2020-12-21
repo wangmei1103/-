7941d01706fb72b4a78692e4f1b61d2b3649bd69
wangmei <2910602199@qq.com>
2020-12-21 20:40:37 +0800

    queue

--- DS/数据结构/20_12_20_queue/20_12_20_queue/queue.c
+++ DS/数据结构/20_12_20_queue/20_12_20_queue/queue.c
@@ -5,46 +5,93 @@ typedef int QDataType;
 
 typedef struct QNode
 { 
- struct QNode* _pNext; 
- QDataType _data; 
+	struct QNode* _next; 
+	QDataType _data; 
 }QNode; 
+
 // 队列的结构
 typedef struct Queue
 { 
 	//头尾指针
 	QNode* _head; 
 	QNode* _tail; 
+	int _size;
 }Queue;
  
 //初始化
 void initQueue(Queue* q)
 {
-
+	if(q == NULL)
+		return;
+	q->_head = q->_tail = NULL;
+	q->_size = 0;
 }
 
-
+//尾插
 void queuePush(Queue* q,QDataType val)
 {
-
+	struct	QNode* node = creatNode(val);
+	if(q->_head == NULL)
+		q->_head = q->_tail = node;
+	else{
+		q->_tail->_next = node;
+		q->_tail=node;
+	}
+	++q->_size;
 }
 
+struct QNode* creatNode(QDataType val)
+{
+	struct QNode* node = (QNode*)malloc(sizeof(QNode));
+	node->_data = val;
+	node->_next = NULL;
+}
 
+//头删
 void queuePop(Queue* q)
 {
-
+	if(q==NULL || q->_head ==NULL)
+		return;
+	struct QNode* next = q->_head->_next;
+	free(q->_head);
+	q->_head=next;
+	--q->_size;
 }
 
 QDataType queueFront(Queue* q)
 {
-
+	return q->_head->_data;
 }
 
 QDataType queueBack(Queue* q)
 {
-
+	return q->_tail->_data;
 }
 
+
+int queueEmpty(Queue* q)
+{
+	return q->_head == NULL;
+}
 int queueSize(Queue* q)
 {
 	//遍历
+	if(q == NULL)
+		return;
+	return q->_size;
+}
+void queueDeatroy(Queue* q)
+{
+	QNode* cur = q->_head;
+	while(cur)
+	{
+		QNode* next = cur->_next;
+		free(cur);
+		cur=q->_head;
+	}
+}
+
+int main()
+{
+	return 0;
 }
